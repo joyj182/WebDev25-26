591b163a44aa5d1cc8e2a55b607affa39e836dc9
joyj182 <joyj18@nycstudents.net>
2026-01-06 19:05:02 +0000

    rw

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,1 +1,20 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+function cat(){
+    document.getElementById("highlight").innerHTML="Cat";
+}
+
+function bird(){
+    document.getElementById("highlight").innerHTML="Bird";
+}
+
+function dog(){
+    document.getElementById("highlight").innerHTML="Dog";
+}
+
+function fish(){
+    document.getElementById("highlight").innerHTML="Fish";
+}
+
+function elephant(){
+    document.getElementById("highlight").innerHTML="Elephant";
+}
