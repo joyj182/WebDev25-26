fe2b67c86c786f12c01b6ae60ff13b04b428cd37
joyj182 <joyj18@nycstudents.net>
2026-01-13 18:55:15 +0000

    v

--- WDLesson3_4Practical/script.js
+++ WDLesson3_4Practical/script.js
@@ -16,8 +16,19 @@ function createStory(){
 
   Choose your variable names wisely.  This is a lot of practice of creating variables and using document.getElementById(..)
   */
-
+  let boy = document.getElementById("boy").value;
+  let girl = document.getElementById("girl").value;
+  let vegetable1 = document.getElementById("veggie1").value;
+  let vegetable2 = document.getElementById("veggie2").value;
   /* Task 4: Set the innerHTML for each of the spans on lines 3 to 9 with the appropriate values from the variables you create in Task 3. */
+
+  boy1span1.innerHTML = boy;
+  boy1span2.innerHTML = boy;
+  boy1span3.innerHTML = boy;
+  boy1span4.innerHTML = boy;
+  veg1.innerHTML = vegetable1;
+  veg2.innerHTML = vegetable2;
+  girlspan.innerHTML = girl;
 
   /* Task 5: Set the display property of the element with an id of "story" to 'block' */
 
