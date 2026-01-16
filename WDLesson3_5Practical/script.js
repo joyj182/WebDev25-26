20eee131e8b2be96233831c4e8f92a791a068fff
joyj182 <joyj18@nycstudents.net>
2026-01-16 19:09:46 +0000

    sda

--- WDLesson3_5Practical/script.js
+++ WDLesson3_5Practical/script.js
@@ -1,26 +1,47 @@
+
 /* Challenge 2: Complete the area and perimeter functions below for the rectangle.  Guidelines,
       1) Create variables and retrieve the information from the text inputs you created in Challenge 1.
+      
       2) Perform the necessary calculations
+
       3) Display the results in the appropriate element
+
 */
 function recArea(){
+      let len = parseFloat(document.getElementById("l").value);
+      let wid = parseFloat(document.getElementById("w").value);
+      let op = document.getElementById("output");
 
+      let A = len * wid;
+      op.innerHTML = A;
 }
 
 function recPerimeter(){
+      let len = parseFloat(document.getElementById("l").value);
+      let wid = parseFloat(document.getElementById("w").value);
+      let out = document.getElementById("output");
 
+      let p = 2*len + 2*wid;
+      out.innerHTML = p;
 }
 
 
 /* Challenge 4: Complete the area and circumference functions below for the circle.  Guidelines,
       1) Create variables and retrieve the information from the text inputs you created in Challenge 3.
+
       2) Perform the necessary calculations
+
       3) Display the results in the appropriate element
+
 */
 let pi = 3.1415926;
 
 function cirArea(){
+      let rad = parseFloat(document.getElementById("r").value);
+      let op2 = document.getElementById("output2");
 
+      let a2 = pi()*rad**2;
+      op2.innerHTML = a2;
 }
 
 function cirPerimeter(){
@@ -29,8 +50,11 @@ function cirPerimeter(){
 
 /* Challenge Bonus: Complete the area and perimeter functions below for the triangle.  Guidelines,
       1) Create variables and retrieve the information from the text inputs you.
+
       2) Perform the necessary calculations
+
       3) Display the results in the appropriate element
+
 */
 function triArea(){
 
