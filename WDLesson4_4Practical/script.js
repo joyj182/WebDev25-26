56a0a67c6cb57563becb68218668a79c608e5fc9
joyj182 <joyj18@nycstudents.net>
2026-02-26 19:08:55 +0000

    a

--- WDLesson4_4Practical/script.js
+++ WDLesson4_4Practical/script.js
@@ -1,15 +1,20 @@
 /* Challenge 2: Complete the function generateProblem by ,
        1) Create a variable for the <span> with id of problem
        2) Generate two random numbers between 0 and 20
-       3) Store the correct answer in the variable correct.  This will be used in checkProblem( )
+       3) Store the correct answer in the variable correct.  This will be used in checkResponse( )
        4) Using string interpolation, display an addition problem like "2 + 2 =" except using the random numbers generated. 
 */
 let correct;
 function generateProblem(){
+       let num1 = Math.floor(Math.random() * (20 - 0 +1) + 0);
+       let num2 = Math.floor(Math.random() * (20 - 0 +1) + 0);
+       let correct = num1 + num2;
+       let problem = document.getElementById("problem");
+       problem.innerHTML = `${num1} + ${num2} = `;
 
 }
 
-/* Challenge 3: Complete the function checkProblem by ,
+/* Challenge 3: Complete the function checkResponse by ,
        1) Create a variable for the users response
        2) Create a variable for the <div> with id of output
        3) Create a variable msg for the output
@@ -18,7 +23,21 @@ function generateProblem(){
            b) otherwise set msg to "Incorrect. The answer is " followed by the correct answer.  Also include incorrect image
        4) Display msg in the output
 */
-function checkProblem(){
+function checkResponse(){
+       let num1 = Math.floor(Math.random() * (20 - 0 +1) + 0);
+       let num2 = Math.floor(Math.random() * (20 - 0 +1) + 0);
+       let correct = num1 + num2
+       let  res = document.getElementById("response").value;
+       msg = "";
+       let op = document.getElementById("output");
+      
+       if (res == correct){
+              msg = "correct.webp";
+       }else{
+              msg = "incorrect.avif";
+       }
+
+        op.innerHTML =`<img src="${msg}">  The answer is ${correct}`;
 
 }
 
